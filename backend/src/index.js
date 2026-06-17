8fdd472f05d8763a3f7b2018b23241a7f1e9cdd8
Bhavya <bhavyas3451@gmail.com>
2026-06-17 21:08:15 +0530

    second commit, want to send this too production

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,20 +2,32 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
 const frontendUrl= process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd() , "public");
+
 app.use(express.json);
 app.use(cors({ origin: frontendUrl , credentials: true}));
 app.use(clerkMiddleware());
-
+ 
 app.get("/health", (req, res) => {
   res.status(200).json({
     message: "Hello from server",
   });
 });
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
