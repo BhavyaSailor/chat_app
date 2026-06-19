00e3866f3b626569f723bb7403b2aad7e4c7d660
Bhavya <bhavyas3451@gmail.com>
2026-06-19 17:58:02 +0530

    model updation

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,6 +1,6 @@
 import express from "express";
-import User from "../models/user.model"
-import {verifyWebhook} from "@clerk/backend/webhooks"
+import User from "../models/user.model.js";
+import { verifyWebhook } from "@clerk/backend/webhooks"
 
 const router = express.Router();
 router.post("/", async (req, res) => {
